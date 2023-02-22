2c6caaa569e977f2d0d2af7e813ebef574c8f463
KSM <ij2356@m-sync.co.kr>
2023-02-22 18:59:18 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Array from "./ArrayState.js";
+import Coin from "./coin.js";
+import "./css/index.css";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-  }
-  ,[])
-
-
-
-  // useEffect(() => {
-  //   fetch("https://jsonplaceholder.typicode.com/posts");
-
-  // }, [])
-  
   return (
-    <div>
-      {/* <Array></Array> */}
-      <h1>The Coin!</h1>
-      {loading ? <strong>loading..</strong> : null}
-      
-    </div>
+    <>
+      <Coin />
+    </>
   );
 }
 
