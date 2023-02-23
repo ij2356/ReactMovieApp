c1610506462d91e2719a2b9f81cafaaf9c151da6
KSM <ij2356@m-sync.co.kr>
2023-02-23 19:00:01 +0900

    s

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import "./css/index.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movieData,setMovieData] = useStatue([])
   useEffect(() => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then((response) => response.json().then((result) => console.log(result.data.movies[1])));
-  },[])
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then((response) => response.json().then((result) => console.log(result)));
+
+  }, [])
+  
+  
+
+
   return (
     <>
       <div>
         {loading ? <h1>loading</h1> : null}
+        {loading ? null : null}
       </div>
     </>
   );
