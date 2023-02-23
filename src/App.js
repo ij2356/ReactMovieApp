b2ff3b91393ed0c2600cbd7deacfca131823eb55
KSM <ij2356@m-sync.co.kr>
2023-02-23 16:37:46 +0900

    git commit 취소연습중

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Coin from "./coin.js";
 import "./css/index.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then((response) => response.json().then((result) => console.log(result.data.movies[1])));
+  },[])
   return (
     <>
-      <Coin />
+      <div>
+        {loading ? <h1>loading</h1> : null}
+      </div>
     </>
   );
 }
--- src/coin.js
+++ src/coin.js
@@ -1,39 +1,41 @@
 import { useEffect, useState } from "react";
 
 const Coin = () => {
-	const [loading, setLoading] = useState(true);
-	const [coins, setCoin] = useState([]);
-	useEffect(() => {
-		fetch("https://api.coinpaprika.com/v1/tickers")
-			.then((response) => response.json())
-			.then((json) => {
-				setCoin(json);
-				setLoading(false);
-			})
-	},[])
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoin] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoin(json);
+        setLoading(false);
+      });
+  }, []);
 
-	function click() {
-		
-		console.log(coins)
-	}
-	return (
+  function click() {
+    console.log(coins);
+  }
+  return (
     <div>
       {/* <Array></Array> */}
-      <h1>The Coin! ({coins.length})</h1>
+      <h1>The Coin! ({loading ? null : coins.length})</h1>
       <button onClick={click}> 콘솔에다가 찍기</button>
-      {loading ? <strong>loading..</strong> : null}
-      <select>
-        {coins.map((coin) => (
-          <option key={coin.id} id={coin.id}>
-            {coin.name}
-            <br />
-            {/* {coin.symbol} */}
-            {coin.quotes.USD.price}
-          </option>
-        ))}
-      </select>
+      {loading ? (
+        <strong>loading..</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id} id={coin.id}>
+              {coin.name}
+              <br />
+              {/* {coin.symbol} */}
+              {coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
-}
+};
 
 export default Coin;
