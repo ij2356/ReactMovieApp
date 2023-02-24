3dc3562d5475df0eb87be5a62ba53e9a1549439e
KSM <ij2356@m-sync.co.kr>
2023-02-24 18:54:37 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 import Array from "./ArrayState.js";
 import Coin from "./coin.js";
 import "./css/index.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [movieData,setMovieData] = useStatue([])
+  const [movieData, setMovieData] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
+    const json = await response.json();
+    setMovieData(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then((response) => response.json().then((result) => console.log(result)));
+    getMovies();
+  }, []);
 
-  }, [])
-  
-  
+  const onClick = () => {
+    console.log(movieData);
+  };
 
-
+  //엑박처리
+  const handleImgError = (e) => {
+    e.target.src = "http://placehold.it/320x100/E8117F/ffffff?text=sample";
+  };
   return (
     <>
       <div>
         {loading ? <h1>loading</h1> : null}
-        {loading ? null : null}
+        <ul>
+          {movieData.map((item, key) => (
+            <div key={item.id}>
+              <div className="thumbnail">
+                <img src={item.medium_cover_image} alt="썸네일" onError={handleImgError} />
+              </div>
+              <h2>
+                {key + 1} {item.title}
+              </h2>
+              <div className="rating">{item.rating}</div>
+
+              {item.hasOwnProperty("genres") ? (
+                <ul>
+                  {item.genres.map((g, index) => (
+                    <li key={index}>{g}</li>
+                  ))}
+                </ul>
+              ) : null}
+            </div>
+          ))}
+        </ul>
+        <button onClick={onClick}>클릭시 콘솔</button>
       </div>
     </>
   );
