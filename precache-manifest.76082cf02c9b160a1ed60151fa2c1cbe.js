75541648bd97c26d8d18a8deb248f9cfb62ae5e9
ij2356 <ij2356@m-sync.co.kr>
2023-02-28 18:21:08 +0900

    Updates

---
+++ precache-manifest.76082cf02c9b160a1ed60151fa2c1cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3be546cec9d1eb226a7",
+    "url": "/ReactMovieApp/static/css/main.04b761ff.chunk.css"
+  },
+  {
+    "revision": "c3be546cec9d1eb226a7",
+    "url": "/ReactMovieApp/static/js/main.c3be546c.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/ReactMovieApp/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "a056fff18b6a1945a7bb",
+    "url": "/ReactMovieApp/static/js/2.a056fff1.chunk.js"
+  },
+  {
+    "revision": "d4c80b1bba0623ef99ce",
+    "url": "/ReactMovieApp/static/js/runtime~main.d4c80b1b.js"
+  },
+  {
+    "revision": "bc1d87368b0a10138296456e784393ef",
+    "url": "/ReactMovieApp/index.html"
+  }
+];
